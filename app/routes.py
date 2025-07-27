e834a8eaa0f21fdb576f5ef948de5775ae97136f
Mariusz <mariusz.lepkowski23@gmail.com>
2025-07-27 22:46:45 +0200

    Add route and service to fetch all albums from the database
    - Implemented GET /albums endpoint to return all albums
    - Created get_all_albums service function to query and return albums ordered by ID
    - Response includes album ID, artist, and title in JSON format

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.services import show_welcome_message
+from app.services import show_welcome_message, get_all_albums
 
 api = Blueprint('api', __name__)
 
@@ -7,3 +7,9 @@ api = Blueprint('api', __name__)
 def home_page():
     welcome_message = show_welcome_message()
     return jsonify(welcome_message), 200
+
+@api.route('/albums', methods=['GET'])
+def show_all_albums():
+    albums = get_all_albums()
+    return jsonify([{"id": a.id, "artist": a.artist, "title": a.title} for a in albums]), 200
+
--- app/services.py
+++ app/services.py
@@ -1,2 +1,10 @@
+from app.models import Album
+from app.extensions import db
+
 def show_welcome_message():
     return {"message": "Welcome to MyAlbums_API!"}
+
+def get_all_albums():
+    all_albums = db.session.execute(db.select(Album).order_by(Album.id)).scalars().all()
+    return all_albums
+    # alternatively  return Album.query.order_by(Album.id).all()
