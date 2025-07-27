a7a8107007e6ad97fc434e538efa87a10bdd6ff7
Mariusz <mariusz.lepkowski23@gmail.com>
2025-07-27 14:36:41 +0200

    Create Album db model.

--- app/models.py
+++ app/models.py
@@ -1,1 +1,6 @@
+from app import db
 
+class Album(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.String(128), nullable=False)
+    title = db.Column(db.String(128), nullable=False)
